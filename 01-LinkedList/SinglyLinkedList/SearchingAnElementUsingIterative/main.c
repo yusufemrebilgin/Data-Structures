e0a3eb7205c4bc354bd9fb5816109263befc5d86
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2023-10-28 18:57:59 +0300

    Create main.c

---
+++ 01-LinkedList/SinglyLinkedList/SearchingAnElementUsingIterative/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+} Node;
+
+struct node *createNode(int data){
+    Node *newNode = malloc(sizeof(Node));
+    if (newNode == NULL) {
+        fprintf(stderr, "Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void add(Node **head, int data){
+    Node *newNode = createNode(data);
+    if (*head == NULL) {
+        newNode->next = *head;
+        *head = newNode;
+    }
+    else {
+        Node *iter = *head;
+        while (iter->next != NULL)
+            iter = iter->next;
+        newNode->next = iter->next;
+        iter->next = newNode;
+    }
+}
+
+bool search(Node *head, int key){
+    if (head == NULL) {
+        printf("List is empty!\n");
+        return false;
+    }
+    while (head != NULL) {
+        if (head->data == key)
+            return true;
+        head = head->next;
+    }
+    return false;
+}
+
+void printList(Node *head){
+    if (head == NULL) 
+        return;
+    while (head != NULL) {
+        printf("->%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main(void){
+
+    int i;
+    Node *head = NULL;
+
+    for (i = 0; i < 5; i++)
+        add(&head, i * 10 + 1);
+
+    printList(head);
+    
+    printf(search(head,11) ? "true\n" : "false\n");
+    printf(search(head,23) ? "true\n" : "false\n");
+
+    return 0;
+}
