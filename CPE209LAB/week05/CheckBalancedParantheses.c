cc347e8733cd852e41f353a41f5ef99c9e6c9b84
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2023-11-10 09:34:57 +0300

    Update CheckBalancedParantheses.c

--- CPE209LAB/week05/CheckBalancedParantheses.c
+++ CPE209LAB/week05/CheckBalancedParantheses.c
@@ -110,8 +110,10 @@ Stack *createStack(size_t size){
             stack->size = size;
         }
         // 'stack->data' için bellek ayırma işlemi başarısız olursa 'stack' için ayrılan bellek serbest bırakılır
-        else
+        else {
             free(stack);
+            return NULL;
+        }
     }
     return stack;
 }
