4491eeb94a8f66e27dbf97cf8593799cff24d450
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-02-14 20:03:40 +0300

    Update main.c

--- 00-ArrayADT/main.c
+++ 00-ArrayADT/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 #include <limits.h>
 
 typedef struct {
@@ -10,19 +11,20 @@ typedef struct {
 } Array;
 
 Array *createArray(size_t initialSize);
-bool isSorted(Array *array);
-bool isEmpty(Array *array);
-bool isFull(Array *array);
+bool isSorted(const Array *array);
+bool isEmpty(const Array *array);
+bool isFull(const Array *array);
 bool isOutOfBounds(size_t index, size_t arrLength);
 bool add(Array *array, int newNum);
 void insert(Array *array, size_t index, int newNum);
-void delete(Array *array, size_t index);
+void deleteByIndex(Array *array, size_t index);
+void deleteByValue(Array *array, int key);
 bool linearSearch(const Array *array, int key);
 bool binarySearch(const Array* array, int key);
 void print(const Array *array, const char *header);
 void fill(Array *array, size_t fromIndex, size_t toIndex, int value);
-int findMax(Array *array);
-int findMin(Array *array);
+int findMax(const Array *array);
+int findMin(const Array *array);
 void swap(int *a, int *b);
 void reverse(Array *array);
 void resize(Array *array, size_t newSize);
@@ -30,14 +32,83 @@ void freeArray(Array **arrayRef);
 
 int main(void) {
 
+    // Test createArray function
+    Array *array = createArray(10);
+    assert(array != NULL);
+
+    // Test add function
+    assert(add(array, 10) == true);
+    assert(add(array, 20) == true);
+    assert(add(array, 30) == true);
+    assert(add(array, 40) == true);
+    assert(add(array, 50) == true);
+
+    // Test insert function
+    insert(array, 1, 15);
+    assert(array->arr[1] == 15 && array->length == 6);
+    print(array, "Array after inserting 15");
+
+    // Test deleteByIndex function
+    deleteByIndex(array, 2);
+    assert(array->length == 5);
+    print(array, "Array after deleting the value in the 2nd index");
+
+    // Test deleteByValue function
+    deleteByValue(array, 10);
+    assert(array->length == 4);
+    print(array, "Array after deleting the value 10");
+    deleteByValue(array, 11);
+    assert(array->length == 4);
+
+    // Test binarySearch function
+    assert(binarySearch(array, 40) == true);
     
+    // Test linearSearch function
+    assert(linearSearch(array, 40) == true);
+    print(array, "Array after linear search function (40) with transposition method");
+    assert(linearSearch(array, 40) == true);
+    print(array, "Array after linear search function (40) with transposition method");
+
+    // Test fill function
+    fill(array, 0, 1, 100);
+    assert(array->arr[0] == 100 && array->arr[1] == 100);
+    print(array, "Array after filling the array from [0] to [1] with the value 100");
+
+    // Test findMax and findMin functions
+    assert(findMax(array) == 100);
+    assert(findMin(array) == 30);
+
+    // Test reverse function
+    reverse(array);
+    print(array, "Array after reversing");
+    assert(array->arr[0] == 50 && array->arr[3] == 100);
+
+    // Test isSorted function
+    assert(isSorted(array) == false);
+
+    // Test isEmpty function
+    assert(isEmpty(array) == false);
+
+    // Test isFull function
+    assert(isFull(array) == false);
+
+    // Test resize function
+    resize(array, 3);
+    print(array, "New array size (3)");
+    assert(array->size == 3);
+
+    // Test freeArray function
+    freeArray(&array);
+    assert(array == NULL);
+
+    printf("All test cases passed successfully!\n");
 
     return 0;
 }
 
 Array *createArray(size_t initialSize) {
-    if (initialSize < 0) {
-        fprintf(stderr, "Size cannot be negative.\n");
+    if (initialSize <= 0) {
+        fprintf(stderr, "Size cannot be zero or negative.\n");
         return NULL;
     }
     Array *array = malloc(sizeof(Array));
@@ -78,7 +149,7 @@ void insert(Array *array, size_t index, int newNum) {
     array->length++;
 }
 
-void delete(Array *array, size_t index) {
+void deleteByIndex(Array *array, size_t index) {
     if (isOutOfBounds(index, array->length)) {
         fprintf(stderr, "Index Out of Bounds\n");
         return;
@@ -87,7 +158,24 @@ void delete(Array *array, size_t index) {
         size_t i;
         for (i = index; i < array->length - 1; i++)
             array->arr[i] = array->arr[i + 1];
+        array->length--;
     }
+}
+
+void deleteByValue(Array *array, int key) {
+    if (isEmpty(array))
+        return;
+
+    size_t i;
+    bool isFound = false;
+    for (i = 0; i < array->length; i++) {
+        if (array->arr[i] == key) {
+            isFound = true;
+            break;
+        }
+    }
+    if (isFound)
+        deleteByIndex(array, i);
 }
 
 bool linearSearch(const Array *array, int key) {
@@ -118,7 +206,7 @@ bool binarySearch(const Array* array, int key) {
 }
 
 void print(const Array *array, const char *header) {
-    if (isEmpty((Array *)array)) 
+    if (isEmpty(array)) 
         return;
     
     size_t i;
@@ -145,10 +233,10 @@ void fill(Array *array, size_t fromIndex, size_t toIndex, int value) {
     }
 }
 
-int findMax(Array *array) {
+int findMax(const Array *array) {
     if (isEmpty(array)) {
         fprintf(stderr, "Array is empty!\n");
-        return INT_MIN;
+        return INT_MAX;
     }
     int max = array->arr[0];
     size_t i;
@@ -159,7 +247,7 @@ int findMax(Array *array) {
     return max;
 }
 
-int findMin(Array *array) {
+int findMin(const Array *array) {
     if (isEmpty(array)) {
         fprintf(stderr, "Array is empty!\n");
         return INT_MIN;
@@ -180,7 +268,8 @@ void swap(int *a, int *b) {
 }
 
 void reverse(Array *array) {
-    if (isEmpty(array)) return;
+    if (isEmpty(array)) 
+        return;
 
     size_t i;
     for (i = 0; i < array->length / 2; i++) {
@@ -188,7 +277,7 @@ void reverse(Array *array) {
     }
 }
 
-bool isSorted(Array *array) {
+bool isSorted(const Array *array) {
     if (isEmpty(array)) {
         fprintf(stderr, "Array is empty!\n");
         return false;
@@ -202,11 +291,11 @@ bool isSorted(Array *array) {
     return true;
 }
 
-bool isEmpty(Array *array) {
+bool isEmpty(const Array *array) {
     return array->length == 0;
 }
 
-bool isFull(Array *array) {
+bool isFull(const Array *array) {
     return array->length == array->size;
 }
 
@@ -217,10 +306,12 @@ bool isOutOfBounds(size_t index, size_t arrLength) {
 void resize(Array *array, size_t newSize) {
     array->arr = realloc(array->arr, sizeof(int) * newSize);
     if (array->arr == NULL) {
-        fprintf(stderr, "Memory reallocation failed for the array!\n");
+        perror("Memory reallocation failed for the array");
         exit(EXIT_FAILURE);
     }
     array->size = newSize;
+    if (newSize < array->length)
+        array->length = newSize;
 }
 
 void freeArray(Array **arrayRef) {
